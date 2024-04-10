0c05345f102a29fc17b995c76bb21db24d9c9418
20211PF.CC0007 <20211PF.CC0007@pc-253372.passofundo.ifsul.local>
2024-04-10 10:10:51 -0300

    10/04/2024

---
+++ src/main/java/br/edu/ifsul/cc/ipoo/compras/lpoo_sistemadanca/model/Pessoas.java
@@ -0,0 +1,68 @@
+package br.edu.ifsul.cc.ipoo.compras.lpoo_sistemadanca.model;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author 20211PF.CC0007
+ */
+public class Pessoas {
+    
+    Integer Id;
+    String nome;
+    String fone;
+    Calendar dataAniver;
+    String email;
+    String endereco;
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer Id) {
+        this.Id = Id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    public Calendar getDataAniver() {
+        return dataAniver;
+    }
+
+    public void setDataAniver(Calendar dataAniver) {
+        this.dataAniver = dataAniver;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
+    
+    
+}
