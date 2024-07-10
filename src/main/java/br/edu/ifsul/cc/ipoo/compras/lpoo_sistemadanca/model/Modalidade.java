db2ca8fd01fe5c42eeaf7837e17bcd0834745f19
Usuario <Usuario@DESKTOP-ON1RKEE>
2024-07-10 05:25:23 -0300

    Merge branch 'main' of https://github.com/LucasMenegol/LPOOSistemaEstudio
    # Conflicts:
    #	src/main/java/br/edu/ifsul/cc/ipoo/compras/lpoo_sistemadanca/model/Pagamento.java
    #	src/test/java/br/edu/ifsul/cc/ipoo/compras/lpoo_sistemadanca/TestePersistenciaDanca.java

--- src/main/java/br/edu/ifsul/cc/ipoo/compras/lpoo_sistemadanca/model/Modalidade.java
+++ src/main/java/br/edu/ifsul/cc/ipoo/compras/lpoo_sistemadanca/model/Modalidade.java
@@ -1,20 +1,22 @@
 package br.edu.ifsul.cc.ipoo.compras.lpoo_sistemadanca.model;
 
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-/**
- *
- * @author 20211PF.CC0007
- */
 @Entity
+@Table(name="tb_modalidade")
 public class Modalidade implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "modalidade_id")
     private Integer id;
+    
+    @Column(nullable = false, length = 155, name = "modalidade_descricao")
     private String descricao;
 
     public Integer getId() {
@@ -32,6 +34,8 @@ public class Modalidade implements Serializable {
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
+    
+    
     
     
 }
